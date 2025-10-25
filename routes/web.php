fe12b321e5d79781c31f76c1a729dd37495ebf8a
John Doe <gamefake@live.nl>
2025-10-25 12:08:34 +0200

    protect /info behind basic dev env middleware
    This way it is not accessible on production

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\RestrictToDevEnv;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,7 +11,7 @@ Route::get('/', function () {
 Route::get('/info', function () {
     Log::info('Phpinfo page visited');
     return phpinfo();
-});
+})->middleware(RestrictToDevEnv::class);
 
 Route::get('/health', function () {
     $status = [];
